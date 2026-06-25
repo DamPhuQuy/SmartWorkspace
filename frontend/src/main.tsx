ebf4b3f6c9c5990c520a1eaa4dc626412ec4c06e
DamPhuQuy <phuquydam06@gmail.com>
2026-06-25 12:11:37 +0700

    frontend init with bun, using tailwindcss and shadcn

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
+import { Agentation } from "agentation";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const ENV = import.meta.env.VITE_ENV;
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    {ENV === "development" && <Agentation />}
   </StrictMode>,
 );
