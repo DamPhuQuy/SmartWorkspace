e8b836cf207c3ebb11dc0354e197802fc2f55412
DamPhuQuy <phuquydam06@gmail.com>
2026-06-27 15:19:59 +0700

    feat(user_entity): create UserEntity and UserProfileEntity

---
+++ backend-java/src/main/java/com/workspace/infrastructure/database/entity/user/UserProfileEntity.java
@@ -0,0 +1,68 @@
+package com.workspace.infrastructure.database.entity.user;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.UuidGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_profiles")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class UserProfileEntity {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator(style = UuidGenerator.Style.VERSION_7)
+    private UUID userId;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @Column(
+        name = "first_name",
+        nullable = false
+    )
+    private String firstName;
+
+    @Column(
+        name = "last_name",
+        nullable = true
+    )
+    private String lastName;
+
+    @Column(
+        name = "avatar_url",
+        nullable = true
+    )
+    private String avatarUrl;
+
+    @Column(
+        name = "bio",
+        nullable = true
+    )
+    private String bio;
+
+    @Column(
+        name = "discord_id",
+        nullable = true,
+        unique = true
+    )
+    private String discordId;
+}
---
+++ docs/BE/CQRS.md
@@ -0,0 +1,6 @@
+# CQRS
+
+CQRS = seperate the flow of writing and reading data.
+
+Command: create/update/delete, changes in data.
+Query: read data, no change in data.
