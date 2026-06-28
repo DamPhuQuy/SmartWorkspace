0ff26e26c32d2eac3dc56056d40b73f6dc14c89f
DamPhuQuy <phuquydam06@gmail.com>
2026-06-28 10:40:58 +0700

    feat(warning): create warning domain, mapper, repository

---
+++ backend-java/src/main/java/com/workspace/domain/model/warning/Warning.java
@@ -0,0 +1,22 @@
+package com.workspace.domain.model.warning;
+
+import java.time.Instant;
+import java.util.UUID;
+import com.workspace.domain.model.workspace.WorkSpaceMember;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Warning {
+    private UUID id;
+    private WorkSpaceMember workspaceMember;
+    private String warningType;
+    private String description;
+    private Instant createdAt;
+    private Instant updatedAt;
+}
